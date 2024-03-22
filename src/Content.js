f5dcb3930db8d99051211438417e8387158ac9dc
shwephuehmone25 <shwephue7889@gmail.com>
2024-03-22 10:41:21 +0630

    update image path

--- src/Content.js
+++ src/Content.js
@@ -18,11 +18,11 @@ import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo1.png";
 import services_logo3 from "./assets/images/Services/logo1.png";
 
-import project1 from "./assets/images/projects/img1.png";
-import project2 from "./assets/images/projects/img2.png";
-import project3 from "./assets/images/projects/img3.png";
-import project4 from "./assets/images/projects/medium.png";
-import project5 from "./assets/images/projects/tiktok.jpg";
+import project1 from "./assets/images/Projects/img1.png";
+import project2 from "./assets/images/Projects/img2.png";
+import project3 from "./assets/images/Projects/img3.png";
+import project4 from "./assets/images/Projects/medium.png";
+import project5 from "./assets/images/Projects/tiktok.jpg";
 import person_project from "./assets/images/projects/person.svg";
 import person2 from "./assets/images/projects/person2.svg";
 
