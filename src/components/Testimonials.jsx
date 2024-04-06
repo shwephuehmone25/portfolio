27a3d4e5c40ac224613cfd8407ffcfc9597eb792
shwephuehmone25 <shwephue7889@gmail.com>
2024-04-06 10:59:26 +0630

    add project demo link to view the project demo

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -35,19 +35,27 @@ const Testimonials = () => {
           modules={[Pagination]}
           className="md:h-96 h-[40rem] max-w-3xl"
         >
+          
           {Testimonials.testimonials_content.map((content, i) => (
             <SwiperSlide key={i}>
               <div
-                className={` duration-500 bg-bg_light_primary mx-8 border-2 
-              p-8 h-full rounded-2xl flex items-center gap-6
-               border-slate-200 md:flex-row flex-col
+                className={`duration-500 bg-bg_light_primary mx-8 border-2 
+                p-8 h-full rounded-2xl flex items-center gap-6
+              border-slate-200 md:flex-row flex-col
                 ${activeIndex !== i && "scale-75 blur-sm"}`}
               >
                 <img src={content.img} alt="..." className="h-24" />
                 <div>
                   <h2 className="text-lg font-medium">{content.name}</h2>
                   <br />
-                  <h6>{content.demo}</h6>
+                  {/* Check if demo value is "#" indicating no link, else render as a link */}
+                  {content.demo !== "#" ? (
+                    <a href={content.demo} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+                      View Project
+                    </a>
+                  ) : (
+                    <span>Link Not Available</span>
+                  )}
                 </div>
               </div>
             </SwiperSlide>
